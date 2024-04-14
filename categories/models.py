a5b8b49ff80fe6ad087ce71a39e7abdc1a98c8d1
Miga <myagmarnaran.n@nomadicss.mn>
2024-04-14 15:53:01 +0800

    connected to mysql

--- categories/models.py
+++ categories/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    created = models.CharField(max_length=200)
+    description = models.TextField()
+    parent = models.ForeignKey("self", on_delete=models.CASCADE)
