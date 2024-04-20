036744461da95ff06584640861e62d362f6efbfe
Miga <myagmarnaran.n@nomadicss.mn>
2024-04-20 15:17:34 +0800

    initial

--- home/views.py
+++ home/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.template import loader
+import re
 
 def home(request):
     template = loader.get_template("home.html")
-    context = {}
-    return HttpResponse(template.render(context, request))
+    context = {
+      'is_login': False
+    }
+    try:
+        match = re.findall(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', request.POST["username"])
+        if len(match) == 1:
+            context = {
+              'is_login': True
+            }
+        return HttpResponse(template.render(context, request))
+    except KeyError:
+        return HttpResponse(template.render(context, request))
